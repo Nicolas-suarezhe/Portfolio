8ec05a239fc7fce571977d9f4a4d3ef9e15446d5
Nicolas-suarezhe <nicolas.suarezhe@gmail.com>
2022-12-13 13:18:39 -0500

    some responsivity changes and cropped logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import HolderComponent from './components/HolderComponent'
+import ParticlesBackground from './components/ParticlesBackground'
 
 
 function App() {
 
   return (
     <div className="App">
+      <ParticlesBackground></ParticlesBackground>
       <HolderComponent/>
     </div>
   )

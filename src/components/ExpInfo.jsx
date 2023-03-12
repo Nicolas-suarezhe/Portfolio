fa32bce3c5267241f6e01b305651dcd984d7de79
Nicolás Alejandro Suárez Henao <nicolassuarezh@pc-9-0-160-190.cm.vtr.net>
2023-03-11 23:16:07 -0500

    project links

--- src/components/ExpInfo.jsx
+++ src/components/ExpInfo.jsx
@@ -17,7 +17,9 @@ const ExpInfo = () => {
                     <h2 className='span' >Chinita web page</h2>
                     <br />
                     <div className='text-and-skills' >
-                        <img src={images.chinitaSS} alt="" className='project-image' />
+                        <a href='' target='_blank'  className="project-image">
+                            <img src={images.chinitaSS} alt="" className='project-image' />
+                        </a>
                         <div>
                             <p className='project-description' >This is a full web page for a small business where I have worked on, it was made alongside a group of students and friends.</p>
                             <h3>Skills applied:</h3>
@@ -34,7 +36,9 @@ const ExpInfo = () => {
                     <h2 className='span' >Rick & Morty wiki</h2>
                     <br />
                     <div className='text-and-skills' >
-                        <img src={images.rickSS} alt="" className='project-image' />
+                    <a href='https://nicosricknmortywiki.netlify.app/' target='_blank'  className="project-image">
+                            <img src={images.rickSS} alt="" className='project-image' />
+                        </a>
                         <div>
                             <p className='project-description' >Here I used an API to create a wiki that can be used to display information about the famous series Rick & Morty</p>
                             <h3>Skills applied:</h3>
@@ -51,7 +55,9 @@ const ExpInfo = () => {
                     <h2 className='span' >Pokedex</h2>
                     <br />
                     <div className='text-and-skills' >
-                        <img src={images.pokedexSS} alt="" className='project-image' />
+                    <a href='https://nicolaspokedex.netlify.app/' target='_blank'  className="project-image">
+                            <img src={images.pokedexSS} alt="" className='project-image' />
+                        </a>
                         <div>
                             <p className='project-description' >Here I used an API to create a pokedex that can be used to display information about your favorite pokemons.</p>
                             <h3>Skills applied:</h3>
@@ -68,7 +74,9 @@ const ExpInfo = () => {
                     <h2 className='span' >Freelancer project</h2>
                     <br />
                     <div className='text-and-skills' >
-                        <img src={images.freelancer} alt="" className='project-image' />
+                    <a href='https://uradvisorproject.netlify.app/' target='_blank'  className="project-image">
+                            <img src={images.freelancer} alt="" className='project-image' />
+                        </a>
                         <div>
                             <p className='project-description' >This is a project where a group of people developed a business idea similar to shutterstock, based on a Colombian small town.</p>
                             <h3>Skills applied:</h3>
